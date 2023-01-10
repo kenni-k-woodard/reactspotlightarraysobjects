00ca70ad3cab9d4811eae82fb0b4398fd8ef3cef
Kenni Woodard <kennikristine@gmail.com>
2023-01-10 13:00:39 -0800

    did one more with the class

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ export const getSum = () => {}
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {}
+export const getFirstTwoArgs = (a, b, ...rest) => {
+  return [a, b, rest]
+}
 
 // INPUT: an object with the following structure
 // {
